64b7e694ffaf23aefcbe20fd729c3b771dda6cb5
masachika <masachika.kmd@gmail.com>
2023-04-03 22:01:01 +0900

    [update] トグルボタンで拡張機能の有効無効を切替

--- script.js
+++ script.js
@@ -2,13 +2,38 @@
 //   console.log("target element:", event.target);
 // });
 
+function handleCtrlEnter(event) {
+  if (
+    event.target.tagName === "TEXTAREA" &&
+    event.code == "Enter" &&
+    !(event.ctrlKey || event.metaKey)
+  ) {
+    event.stopPropagation();
+  }
+}
+
+function enableSendingWithCtrlEnter() {
+  document.addEventListener("keydown", handleCtrlEnter, { capture: true });
+}
+
+function disableSendingWithCtrlEnter() {
+  document.removeEventListener("keydown", handleCtrlEnter, { capture: true });
+}
+
 chrome.storage.sync.get("isEnabled", (data) => {
   const isEnabled = data.isEnabled;
   if (isEnabled) {
-    document.addEventListener("keydown", (event) => {
-      if (event.target.tagName === "TEXTAREA" && event.code == "Enter" && !(event.ctrlKey || event.metaKey)) {
-        event.stopPropagation();
-      }
-    }, { capture: true });
+    enableSendingWithCtrlEnter();
+  }
+});
+
+chrome.storage.onChanged.addListener((changes, area) => {
+  if (area === "sync" && "isEnabled" in changes) {
+    const isEnabled = changes.isEnabled.newValue;
+    if (isEnabled) {
+      enableSendingWithCtrlEnter();
+    } else {
+      disableSendingWithCtrlEnter();
+    }
   }
 });
