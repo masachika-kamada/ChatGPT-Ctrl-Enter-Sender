05eec7a8c1547d17001daedd05bb249fc4b897f9
momosuke <rusk.ryoy4n@gmail.com>
2023-05-15 16:44:22 +0900

    [add] Bardを追加

--- background.js
+++ background.js
@@ -4,7 +4,8 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     const isEnabled = data.isEnabled !== undefined ? data.isEnabled : true;
     if (url && (url.startsWith("https://chat.openai.com") ||
                 url.startsWith("https://poe.com") ||
-                url.startsWith("https://www.phind.com"))) {
+                url.startsWith("https://www.phind.com") ||
+                url.startsWith("https://bard.google.com"))) {
         if (changeInfo.status === "complete") {
           chrome.action.setIcon({ path: isEnabled ? "icon/enabled.png" : "icon/disabled.png" });
           chrome.action.enable(tabId);
--- popup.js
+++ popup.js
@@ -18,7 +18,8 @@ function updateIcon() {
     const url = tabs[0].url ?? "";
     if (url.startsWith("https://chat.openai.com") ||
         url.startsWith("https://poe.com") ||
-        url.startsWith("https://www.phind.com")) {
+        url.startsWith("https://www.phind.com") ||
+        url.startsWith("https://bard.google.com")) {
       chrome.action.setIcon({ path: isEnabled ? "icon/enabled.png" : "icon/disabled.png" });
     } else {
       chrome.action.setIcon({ path: "icon/na.png" });
