5919e3162120a97f3d2db8c4470131f2efe437fd
masachika <masachika.kmd@gmail.com>
2023-05-25 01:07:41 +0900

    [update] 対応サイトにChatPDFとPerplexityを追加

--- background.js
+++ background.js
@@ -5,7 +5,9 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (url && (url.startsWith("https://chat.openai.com") ||
                 url.startsWith("https://poe.com") ||
                 url.startsWith("https://www.phind.com") ||
-                url.startsWith("https://bard.google.com"))) {
+                url.startsWith("https://bard.google.com") ||
+                url.startsWith("https://www.chatpdf.com") ||
+                url.startsWith("https://www.perplexity.ai"))) {
         if (changeInfo.status === "complete") {
           chrome.action.setIcon({ path: isEnabled ? "icon/enabled.png" : "icon/disabled.png" });
           chrome.action.enable(tabId);
