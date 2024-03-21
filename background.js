bb59b19165d30daa5ed075e6dac00e7c4435085a
Juris710 <juris710.dev@gmail.com>
2024-03-21 13:19:18 +0900

    [add] support claude.ai in popup.js and background.js

--- background.js
+++ background.js
@@ -7,7 +7,8 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
                 url.startsWith("https://www.phind.com") ||
                 url.startsWith("https://chat.mistral.ai") ||
                 url.startsWith("https://www.chatpdf.com") ||
-                url.startsWith("https://www.perplexity.ai"))) {
+                url.startsWith("https://www.perplexity.ai") ||
+                url.startsWith("https://claude.ai"))) {
         if (changeInfo.status === "complete") {
           chrome.action.setIcon({ path: isEnabled ? "icon/enabled.png" : "icon/disabled.png" });
           chrome.action.enable(tabId);
