003c37134bd6032f9c6b6f872702717e2bf3284f
changx <changx@gmail.com>
2024-08-22 15:28:54 +0800

    add: compitable with keyup handlers like getliner.com

--- background.js
+++ background.js
@@ -12,6 +12,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
                 url.startsWith("https://claude.ai") ||
                 url.startsWith("https://www.bing.com/chat") ||
                 url.startsWith("https://you.com") ||
+                url.startsWith("https://getliner.com") ||
                 url.startsWith("https://dashboard.cohere.com/playground/chat"))) {
         if (changeInfo.status === "complete") {
           chrome.action.setIcon({ path: isEnabled ? "icon/enabled.png" : "icon/disabled.png" });
