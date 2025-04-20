ed0491356ab4f0bf77bfce517970dc9657399cf8
Masachika Kamada <63488322+masachika-kamada@users.noreply.github.com>
2025-04-21 01:07:22 +0900

    [refactor] Extract common Ctrl+Enter logic into ctrl-enter-utils.js

--- ctrl_enter_chatgpt.js
+++ ctrl_enter_chatgpt.js
@@ -51,33 +51,6 @@ function handleCtrlEnter(event) {
   }
 }
 
-function enableSendingWithCtrlEnter() {
-  document.addEventListener("keydown", handleCtrlEnter, { capture: true });
-}
-
-function disableSendingWithCtrlEnter() {
-  document.removeEventListener("keydown", handleCtrlEnter, { capture: true });
-}
-
-function getHostname() {
-  return window.location.hostname;
-}
-
-function applySiteSetting() {
-  const hostname = getHostname();
-
-  chrome.storage.sync.get("siteSettings", (data) => {
-    const settings = data.siteSettings || {};
-    const isEnabled = settings[hostname] ?? false;
-
-    if (isEnabled) {
-      enableSendingWithCtrlEnter();
-    } else {
-      disableSendingWithCtrlEnter();
-    }
-  });
-}
-
 // Apply the setting based on the current site on initial load
 applySiteSetting();
 
